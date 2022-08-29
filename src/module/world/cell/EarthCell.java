f99164846c8de4306e86cb027713a24c0f4fb0d1
Porokhnya <Porokhnya.A.N@yandex.kz>
2022-08-29 15:47:31 +0600

    ADD NEW CLASS FACTORY.

--- src/module/world/cell/EarthCell.java
+++ src/module/world/cell/EarthCell.java
@@ -1,8 +1,13 @@
 package module.world.cell;
 
+import module.animal.Animal;
 import module.world.Create;
 
+import java.util.List;
+import java.util.Map;
+
 public class EarthCell extends Cell implements Create {
+    Map<String, List<Animal>> mapAnimal;
     @Override
     public void create() {
 
