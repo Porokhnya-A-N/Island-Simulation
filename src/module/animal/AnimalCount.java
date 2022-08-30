456c3b408b452bd98c663f6e28e6d02cffa3df99
Porokhnya <Porokhnya.A.N@yandex.kz>
2022-08-30 11:29:35 +0600

    create enum class AnimalCount

---
+++ src/module/animal/AnimalCount.java
@@ -0,0 +1,39 @@
+package module.animal;
+
+public enum AnimalCount {
+//Кабан.
+     MAX_BOAR (50),
+    //Буйвол
+    MAX_BUFFALO (10),
+    //Гусеница
+      MAX_CATERPILLAR  (1000),
+    //Олень
+    MAX_DEER (20),
+    //Утка
+     MAX_DUCK  (200),
+    //Коза
+    MAX_GOAT  (140),
+    //Лошадь
+     MAX_HOURS  (20),
+    //Мышь
+     MAX_MOUSE  (500),
+    //Кролик
+     MAX_RABBIT  (150),
+    //Овца
+   MAX_SHEEP  (140),
+    //Медведь
+    MAX_BEAR ( 5 ),
+    //Удав
+     MAX_BOA  (30 ),
+    //Орел
+    MAX_EAGLE  (20),
+    //Лиса
+     MAX_FOX  (30 ),
+    //Волк
+    MAX_WOLF (30);
+    //Растения.
+
+    AnimalCount(int i) {
+
+    }
+}
--- src/module/world/cell/EarthCell.java
+++ src/module/world/cell/EarthCell.java
@@ -2,17 +2,30 @@ package module.world.cell;
 
 import module.animal.Animal;
 import module.animal.AnimalType;
+import module.generation.Generation;
+import module.generation.factory.AnimalFactory;
 import module.world.Create;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class EarthCell extends Cell implements Create {
+public class EarthCell extends Cell implements Create, Generation {
     private AnimalType plantsAndAnimal;
-    Map<String, List<Animal>> mapAnimal;
+    Map<String, List<Animal>> mapAnimal = new HashMap<>();
+    public AnimalFactory animalFactory = new AnimalFactory();
     @Override
     public void create() {
+        createLiveInCell();
 
+    }
+    private void createLiveInCell(){
+        AnimalType types[] = plantsAndAnimal.values();
+
+        for(AnimalType type : types){
+            System.out.println(type);
+
+        }
     }
 }
 
