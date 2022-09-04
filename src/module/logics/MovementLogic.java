b2b80de007f01de68651ea2e9f9c353342b4c2b5
Porokhnya <Porokhnya.A.N@yandex.kz>
2022-09-04 16:52:27 +0600

    completion logic move

--- src/module/logics/MovementLogic.java
+++ src/module/logics/MovementLogic.java
@@ -4,21 +4,68 @@ import module.animal.Animal;
 import module.world.cell.EarthCell;
 
 public class MovementLogic {
-    int x, y;
+    private int x, y;
     public void startMove(int direction, int corX, int corY, Animal object, EarthCell[][] earthCell){
-        x = corX; y = corY;
+      x = corX;
+      y = corY;
         int steep = object.toGenerate(object.getSpeed());
         if(direction == 0 && steep!=0){
-           if(earthCell.length > (corX + steep){
-               startMoveCorX(x,y,object,earthCell,corX + steep));
-            } else if () {
+           if(earthCell.length >= (x + steep)){
+               while(steep > 0){
+               if(isSteepOnRight(earthCell.length,steep)) {
+                    startMoveCorX(y, object, earthCell, x + steep);
+                    break;
+                }else{
+                   steep--;
+               }
 
+               }
+            } else if (earthCell.length < (x + steep)) {
+                while(steep > 0){
+                    if(isSteepOnLeft(steep)){
+                        startMoveCorX(y, object, earthCell, x - steep);
+                        break;
+                    }else {
+                        steep--;
+                    }
+
+                }
 
            }
-        }else if (direction == 1){
-
+        }else if (direction == 1 && steep !=0){
+                if(earthCell[x].length >= (y + steep)){
+                    while (steep>0){
+                        if(isSteepOnBottom(earthCell[x].length,steep)){
+                            startMoveCorY(x,object,earthCell,steep);
+                            break;
+                        }else {
+                            steep--;
+                        }
+                    }
+                }else if(earthCell[x].length < (y + steep)){
+                    while(steep>0){
+                        if(isSteepOnTop(steep)){
+                            startMoveCorY(x,object,earthCell,steep);
+                            break;
+                        }else{
+                            steep--;
+                        }
+                    }
+                }
         }
 
+    }
+    private boolean isSteepOnLeft( int steep){
+        return (x - steep ) >= 0;
+    }
+    private boolean isSteepOnRight(int length, int steep){
+        return (length - ( x+ steep )) >= steep;
+    }
+    private boolean isSteepOnTop(int steep){
+        return (y - steep ) >= 0;
+    }
+    private boolean isSteepOnBottom(int length, int steep){
+        return (length - ( y+ steep )) >= steep;
     }
     public void startMoveCorX( int corY,Animal object, EarthCell[][] earthCell, int steep ){
         if(earthCell[steep][corY].getMapAnimal().get(object.getAnimalType()).size()< object.getMaxAnimal()) {
